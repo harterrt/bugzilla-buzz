e2b1bc6fa155480e63fe3ffb5546aa5dc5f76b39
Ryan Harter <harterrt@gmail.com>
2016-12-09 15:07:49 -0500

    Make start date variable

--- python/main.py
+++ python/main.py
@@ -1,18 +1,20 @@
-from datetime import date
+from datetime import date, timedelta
 import requests
 import json
 
 # TODO: harterrt:
-# * Set default date to one week ago
+# x Set default date to friday at least one week ago
 # * Add flag to override default active-since date
-# * Add changes/comments
+# * Figure out how to package the CLI - pinboard links
+# * Add changes/comments overview
+ 
 def main():
     response = requests.get(
         "https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary" +
         "&email1=rharter@mozilla.com" +
         "&emailassigned_to1=1" +
         "&emailcc1=1"
-        "&last_change_time=2016-12-01"
+        "&last_change_time={start_date}".format(start_date=get_start_day())
         )
     bugs = json.loads(response.text)['bugs']
 
@@ -21,5 +23,12 @@ def main():
 
     return bugs
 
-# if __name__ == "__main__":
-#     main()
+def get_start_day(today = date.today()):
+    # Decide how far back we want to look for "fresh" bugs
+    # Returns Friday, at least a week ago
+    last_friday = today - timedelta(((today.weekday() + 3) % 7) + 7)
+
+    return last_friday.isoformat()
+
+if __name__ == "__main__":
+    main()
