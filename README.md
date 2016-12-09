f060d4d2b9c5b870fbf4810551f52c27fc606a26
Ryan Harter <harterrt@gmail.com>
2016-12-08 22:23:41 -0500

    Add todos and change fmt

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ The more useful your bugs are the more you'll want to keep them up to date.
 # How to Use
 You can't. I haven't written any code yet.
 
+# Goals
+* Identify all bugs I'm CC'd, Assigned, or mentioned in. (relevant bugs)
+* Identify recently modified relevant bugs
+* Identify zombie bugs
+
 # Heads up
 This is an early proof of concept for personal use.
 Don't expect this to be stable or usable just yet.
--- python/main.py
+++ python/main.py
@@ -1,18 +1,23 @@
+from datetime import date
 import requests
 import json
 
+# TODO: harterrt:
+# * Set default date to one week ago
+# * Add flag to override default active-since date
+# * Add changes/comments
 def main():
     response = requests.get(
         "https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary" +
         "&email1=rharter@mozilla.com" +
         "&emailassigned_to1=1" +
         "&emailcc1=1"
-        "&last_change_time=2016-08-28"
+        "&last_change_time=2016-12-01"
         )
     bugs = json.loads(response.text)['bugs']
 
     for bug in bugs:
-        print "Bug {id}: {summary}".format(**bug)
+        print "[Bug {id}](http://bugzil.la/{id}): {summary}".format(**bug)
 
     return bugs
 
